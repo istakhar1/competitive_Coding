941abd5e6278f8846d82c0f9acabcd1e60206e2c
istakhar <istakhar054@gmail.com>
2022-08-08 22:22:42 +0530

    fibo of n using matrix exponentiation

---
+++ Amazon/matrix+exponention.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+const int mod = 1e9+7;
+const int sz =2;
+struct Mat{
+    int m[sz][sz];
+    Mat(){
+        memset(m,0,sizeof(m));
+    }
+    //create a fn which convert a matrix to identity matrix;
+    
+    void identity(){
+        for(int i=0;i<sz;i++){
+            m[i][i]=1;
+        }
+    }
+    // multiply two matrix using operatoer overloadindg
+    
+    Mat operator*(Mat a){
+        Mat res;
+        for(int i=0;i<sz;i++){
+            for(int j=0;j<sz;j++){
+                for(int k=0;k<sz;k++){
+                    res.m[i][j]+=m[i][k] * a.m[k][j];
+                    res.m[i][j]%=mod;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int fib(int n){
+    Mat res;
+    res.identity();
+    Mat T;
+    T.m[0][0] =T.m[0][1] =T.m[1][0]=1;
+    if(n<=2) return 1;
+    n-=2;
+    while(n){
+        if(n&1)res =res * T;
+        T=T*T;
+        n/=2;
+    }
+    return (res.m[0][0]+res.m[0][1])%mod;
+}
+
+int main() {
+	// your code goes here
+	int n=5;
+	cout<<fib(30);
+	return 0;
+}
